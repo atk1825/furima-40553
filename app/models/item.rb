e3750636e8cb131074c494693e93963cb985895b
atk1825 <akkun1825@yahoo.co.jp>
2024-06-14 17:11:42 +0900

    ActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
-  belongs_to :area_id
-  belongs_to :load_id
-  belongs_to :delivery_id
+  belongs_to :area
+  belongs_to :load
+  belongs_to :delivery
   belongs_to :user
+  has_one_attached :image
 
   validates :category_id, :condition_id, :area_id, :load_id, :delivery_id,
             numericality: { other_than: 1, message: "can't be blank" },
