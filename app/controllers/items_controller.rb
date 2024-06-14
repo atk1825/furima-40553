bb79cde31f29d04213c85da03fc147514af432da
atk1825 <akkun1825@yahoo.co.jp>
2024-06-14 14:37:54 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,14 +2,31 @@ class ItemsController < ApplicationController
   before_action :move_to_index, only: [:new]
 
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path(@item)
+    else
+      render new, status: :unprocessable_entity
+    end
+  end
+
+  private
 
   def move_to_index
     unless user_signed_in?
-      redirect_to action: :index
+      redirect_to new_user_session_path
     end
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :content, :image, :price).merge(user_id: current_user.id)
   end
 end
