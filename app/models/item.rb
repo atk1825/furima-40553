5af472b2e46ea57c8c4f92b0272452727337ccab
atk1825 <akkun1825@yahoo.co.jp>
2024-06-15 16:43:20 +0900

    バリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,9 +8,19 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validate  :was_attached?
+  validates :name, presence: true
+  validates :content, presence: true
   validates :category_id, :condition_id, :area_id, :load_id, :delivery_id,
             numericality: { other_than: 1, message: "can't be blank" },
             presence: true
-  validates :name, presence: true
-  validates :content, presence: true
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                    format: { with: /\d/, message: "is invalid. Input half-width characters." }
+
+  private
+
+  def was_attached?
+    errors.add(:image, "can't be blank") unless image.attached?
+  end
 end
