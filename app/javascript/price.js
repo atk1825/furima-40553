2338c2fc44530f3e1b4798ef239e9d60f5e854a2
atk1825 <akkun1825@yahoo.co.jp>
2024-06-15 10:32:58 +0900

    JSの実装

---
+++ app/javascript/price.js
@@ -0,0 +1,14 @@
+function tax() {
+  const price = document.getElementById("item-price");
+  price.addEventListener('keyup',() =>{
+    const priceValue = parseFloat(price.value) || 0;
+    const priceTax = priceValue * 0.1;
+    const addTax  = document.getElementById("add-tax-price");
+    addTax.innerHTML = `${Math.floor(priceTax)}`;
+
+    const priceProfit = priceValue - priceTax;
+    const addProfit  = document.getElementById("profit");
+    addProfit.innerHTML =`${Math.floor(priceProfit)}`;
+  })  
+}
+window.addEventListener('load', tax);
