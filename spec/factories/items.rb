941d843d7c98109fe49be84b3f451c3bd801ed88
atk1825 <akkun1825@yahoo.co.jp>
2024-06-15 16:42:25 +0900

    テストコードの作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    name            { 'テストアイテム' }
+    content         { 'テスト内容' }
+    category_id     { 3 }
+    condition_id    { 3 }
+    load_id         { 3 }
+    area_id         { 3 }
+    delivery_id     { 3 }
+    price           { 500 }
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/item-sample.png'), filename: 'test_image.png')
+    end
   end
 end
