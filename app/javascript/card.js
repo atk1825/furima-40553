c47e22d7c0faf01af2c18f6c742f5f430124d2dd
atk1825 <akkun1825@yahoo.co.jp>
2024-06-21 14:43:56 +0900

    Update card.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,5 @@
 const pay = () => {
   const publickey = gon.public_key
-
   const payjp = Payjp(publickey)
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
