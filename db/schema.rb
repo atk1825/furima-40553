8a823cb4612976bd07a1240da6e1c93e8e0e7cb7
atk1825 <akkun1825@yahoo.co.jp>
2024-08-22 14:25:47 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_17_045004) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "categories", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "content", null: false
